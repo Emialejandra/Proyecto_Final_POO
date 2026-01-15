68b5dc09839cb3485581df52c54a558b4c08b82e
Emialejandra <emilia.tana.p@gmail.com>
2026-01-14 20:40:17 -0500

    estructura inicial del proyecto

---
+++ src/test/java/com/example/proyecto_final_poo/ProyectoFinalPooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.proyecto_final_poo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoFinalPooApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
